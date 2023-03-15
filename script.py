cba14ba66104359cce7e10a6b40cfc1622c73f17
WinterKRALLe <82891169+WinterKRALLe@users.noreply.github.com>
2023-03-15 09:47:57 +0100

    update

--- script.py
+++ script.py
@@ -4,24 +4,31 @@ from pyzbar.pyzbar import decode
 from pdf2image import convert_from_path
 from PyPDF3 import PdfFileWriter, PdfFileReader
 
+pdf_dir = "/home/winter/Documents"
+new_dir = "/home/winter/Pictures"
+
 def Extract_Code_From_PDF(input_file, output_file, code_type):
+    
     output = PdfFileWriter()
     input1 = PdfFileReader(open(input_file, "rb"))
+
     output_page = input1.getPage(0)
 
-    # bar code
+    #bar code
     if 'bar' in code_type.lower():
         output_page.cropBox.lowerLeft = (0, 0)
         output_page.cropBox.upperleft = (0, 100)
         output_page.cropBox.lowerRight = (286, 0)
         output_page.cropBox.upperRight = (286, 100)
 
-    # Data Matrix code
+
+    #Data Matrix code
     # if 'matrix' in code_type.lower():
     #     output_page.cropBox.lowerLeft = (200, 309)
     #     output_page.cropBox.upperleft = (200, 378)
     #     output_page.cropBox.lowerRight = (270, 309)
     #     output_page.cropBox.upperRight = (270, 378)
+
 
     output.addPage(output_page)
     outputStream = open(output_file, "wb")
@@ -29,19 +36,32 @@ def Extract_Code_From_PDF(input_file, output_file, code_type):
 
 
 def rename(input_file):
-    output_file = f"{pdf_dir}/outpyBar.pdf"
+
+    output_file = rf"{pdf_dir}/outpyBar.pdf"
+
+    # code_type = 'data matrix code'
     code_type = 'bar code'
+
 
     Extract_Code_From_PDF(input_file, output_file, code_type)
 
+
     pages = convert_from_path(output_file, dpi=300, first_page=0, last_page=0)
     image = pages[0]
+
+
     image_path = f"{pdf_dir}/page.png"
+
     image.save(image_path)
 
     image = cv2.imread(image_path)
+
+
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+
     barcodes = decode(gray_image)
+
 
     for barcode in barcodes:
         (x, y, w, h) = barcode.rect
@@ -49,14 +69,15 @@ def rename(input_file):
         print("Barcode data:", barcode.data.decode("utf-8"))
         print("Barcode type:", barcode.type)
 
-        new_file_path = os.path.join(os.path.expanduser(pdf_dir), bar)
-        os.rename(input_file, new_file_path)
+    new_file_path = os.path.join(os.path.expanduser(new_dir), bar + ".pdf")
+    os.rename(input_file, new_file_path)
 
-        os.remove(image_path)
-        os.remove(output_file)
+    os.remove(image_path)
+    os.remove(output_file)
+    
 
 
-pdf_dir = os.environ.get('PDF_DIR')
+
 
 for filename in os.listdir(pdf_dir):
     if filename.endswith(".pdf"):
